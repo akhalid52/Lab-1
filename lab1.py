d44dbff2d4b67f21a5138c6b44b787b9ff37c3a7
akhalid52 <akhalid52@myseneca.ca>
2024-02-12 21:09:38 -0500

    Add try-except

--- lab1.py
+++ lab1.py
@@ -1,10 +1,13 @@
-def calculating_age():
-    birth_year = int(input("Enter your birth year: "))
-    current_year = 2024
-    age = current_year - birth_year
-    print(f"You are {age} years old.")
+def calculate_age():
+    try:
+        birth_year = int(input("Enter your birth year: "))
+        current_year = 2024
+        age = current_year - birth_year
+        print(f"You are {age} years old.")
+    except ValueError:
+        print("Please enter a valid integer for the birth year.")
 
-calculating_age()
+calculate_age()
 
 def helloWorld():
 	print(‘Hello World’)
